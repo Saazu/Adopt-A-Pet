74af2ac517804b6b883a13e32bf75f928fbf7053
Alfred Delle <alfred.delle@yale.edu>
2020-04-01 22:56:56 +0000

    added jsx

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-const Pet = ({ animal, name, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {}, //attributes
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Adu",
-        animal: "Dog",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        name: "Poku",
-        animal: "Dog",
-        breed: "Bulldog",
-      }),
-      React.createElement(Pet, {
-        name: "Kwabena",
-        animal: "Dog",
-        breed: "Shepherd",
-      }),
-    ]
-    //children
+  return (
+    <div>
+      <h1>Adopt me</h1>
+      <SearchParams />
+    </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));

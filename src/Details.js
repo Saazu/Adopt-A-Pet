a4894b9f2a483fb6b3784f6c5ddbac36e0d54cbf
Alfred Delle <alfred.delle@yale.edu>
2020-04-06 18:46:10 +0000

    resolving git issues

--- src/Details.js
+++ src/Details.js
@@ -7,7 +7,6 @@ class Details extends React.Component {
   state = { loading: true };
 
   componentDidMount() {
-    throw new Error("hahaha");
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
